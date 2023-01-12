64e8f8a07bff2d56669fd8d1ef526d25a5dbfe4d
luiz-cazarin <57879201+luiz-cazarin@users.noreply.github.com>
2023-01-11 22:45:54 -0300

    feat: reactDOM portal

--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('backdrop-root'));
 root.render(<App />);
